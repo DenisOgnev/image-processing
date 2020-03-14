baa009b8eb7f97619742da95693c563ea5dcecd5
Огнев Денис Вячеславович <ognev.d@ccam.cmc.unn.net>
2020-03-14 12:06:40 +0300

    Glass filter

---
+++ computer graphics 1/Classes/LinerTension.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace computer_graphics_1
+{
+    class LinerTension : Filters
+    {
+        protected override Color calculateNewPixelColor(Bitmap sourceImage, int x, int y)
+        {
+            Color sourceColor = sourceImage.GetPixel(x, y);
+            Color resultColor;
+            throw new NotImplementedException();
+        }
+    }
+}
